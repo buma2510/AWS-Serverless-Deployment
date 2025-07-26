c066fc47a0fdce9ed2fcb89c70e50c3a43771a8d
Randy-M <142512902+buma2510@users.noreply.github.com>
2025-07-26 01:45:47 +0100

    Update API-test-script.sh

--- scripts/API-test-script.sh
+++ scripts/API-test-script.sh
@@ -1,14 +1,1 @@
-# Create API test script
-cat <<EOF > /home/ec2-user/test_api.sh
-#!/bin/bash
-API_URL="$(aws ssm get-parameter --name "/greeting-api/url" --query "Parameter.Value" --output text)"
-TOKEN=\$(aws cognito-idp initiate-auth \
-  --client-id \$(aws ssm get-parameter --name "/greeting-api/cognito-client-id" --query "Parameter.Value" --output text) \
-  --auth-flow USER_PASSWORD_AUTH \
-  --auth-parameters USERNAME=user@example.com,PASSWORD=Passw0rd! | jq -r '.AuthenticationResult.IdToken')
 
-curl -H "Authorization: Bearer \$TOKEN" "\$API_URL?name=EC2User"
-EOF
-
-chmod +x /home/ec2-user/test_api.sh
-chown ec2-user:ec2-user /home/ec2-user/test_api.sh
